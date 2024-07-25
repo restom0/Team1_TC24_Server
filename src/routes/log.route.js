b51f182a7a060a2e5ad7496d6b4c70e0f757cb7f
Thái Ngọc Rạng <98004370+restom0@users.noreply.github.com>
2024-07-25 20:59:56 +0700

    fix bug

--- src/routes/log.route.js
+++ src/routes/log.route.js
@@ -1,17 +1,6 @@
 import express from 'express'
 import { LogController } from '../controllers/log.controller.js'
 const LogRouter = express.Router()
-/**
- * @swagger
- * /logs:
- *  get:
- *   tags:
- *    - Log
- *   summary: Get log data
- *   description: Get log data
- *  responses:
- *   200:
- *   description: Success
- */
+
 LogRouter.get('/', LogController.getDataOnDate)
 export { LogRouter }
--- swagger.js
+++ swagger.js
@@ -2,23 +2,42 @@ import swaggerAutogen from 'swagger-autogen'
 
 const doc = {
   info: {
-    title: 'My API',
-    description: 'Description'
+    version: '', // by default: '1.0.0'
+    title: '', // by default: 'REST API'
+    description: '' // by default: ''
   },
-  host: 'localhost:7000'
+  servers: [
+    {
+      url: 'https://team1-tc24-server.onrender.com',
+      description: '' // by default: ''
+    }
+    // { ... }
+  ],
+  tags: [
+    // by default: empty Array
+    {
+      name: '', // Tag name
+      description: '' // Tag description
+    }
+    // { ... }
+  ],
+  components: {} // by default: empty object
 }
 
 const outputFile = './swagger-output.json'
 const routes = [
-  './src/routes/log.route.js',
-  './src/routes/restaurant.route.js',
-  './src/routes/table.route.js',
-  './src/routes/order.route.js',
-  './src/routes/user.route.js',
-  './src/routes/menu.route.js'
+  // './src/routes/log.route.js',
+  // './src/routes/restaurant.route.js',
+  // './src/routes/table.route.js',
+  // './src/routes/order.route.js',
+  // './src/routes/user.route.js',
+  // './src/routes/menu.route.js'
+  './server.js'
 ]
 
 /* NOTE: If you are using the express Router, you must pass in the 'routes' only the 
 root file where the route starts, such as index.js, app.js, routes.js, etc ... */
 
-swaggerAutogen()(outputFile, routes, doc)
+swaggerAutogen()(outputFile, routes, doc).then(async () => {
+  await import('./server.js') // Your project's root file
+})
