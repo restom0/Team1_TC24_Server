23bb1c120af4b450cd9d5d37af9cc92692f13894
Thái Ngọc Rạng <98004370+restom0@users.noreply.github.com>
2024-07-26 19:03:52 +0700

    fix bug

--- src/dto/in/user.dto.js
+++ src/dto/in/user.dto.js
@@ -26,7 +26,7 @@ const UserRegisterValidation = [
     .withMessage('Password is required')
     .isString()
     .withMessage('Password must be a string')
-    .matches(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/)
+    .matches(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z])(?=.*[!@#$%^&*(),.?":{}|<>]).{8,}$/)
     .withMessage(
       'Password must be at least 8 characters long, contain at least one number, one capital letter, one lowercase letter and one special character'
     ),
